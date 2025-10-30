b522bae9cfba3306dff8f1f288512aac3772f370
Bushra01-Dhaka <humayraanjum87@gmail.com>
2025-10-30 21:43:08 +0600

    Today I have learned gsap functionality of to, from and also learn some powerful property like duration, delay, stagger, yoyo, repeat

--- Day1/script.js
+++ Day1/script.js
@@ -17,3 +17,35 @@ gsap.from("#box2", {
     borderRadius:"50%",
     backgroundColor:"blue",
 })
+
+gsap.to("img", {
+    rotate:360,
+    duration:3,
+    delay:1,
+    scale:1,
+    borderRadius:"50%",
+})
+
+gsap.from("h2",{
+    y:500,
+    duration:2,
+    delay:1,
+})
+
+gsap.from("p",{
+    y:500,
+    duration:2,
+    delay:1,
+    color:"red"
+})
+
+gsap.from("h1", {
+    opacity:0,
+    y:30,
+    duration:1,
+    delay:1,
+    color:red,
+    stagger:1
+
+})
+
